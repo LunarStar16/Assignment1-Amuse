cf3854b6005de090f8ae05ea15cdd0625692140d
lexa <liam.wright@uleth.ca>
2022-10-31 11:53:19 -0600

    start customization

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -8,21 +8,30 @@ var mainView = app.views.create('.view-main')
 //https://happycoding.io/examples/p5js/input/mouse-ripple
 
 
-var Size = 5;
+var Speed = 5;
 var ripples = [];
+var Size = 5;
+var backcolour = 255;
+var strokecolour = '#000';
+
+$("#stroke").on("change", function(){
+strokecolour = $('#stroke').val();
+
+
+})
 
 
 function setup() {
   var cnv = createCanvas(windowWidth, windowHeight);
   cnv.parent("p5canvas");
   noFill();
-  strokeWeight(5);
-
+  strokeWeight(Size);
+  
 }
 
 function draw() {
-  background(255, 25);
-  
+  background(backcolour, 15);
+  stroke(strokecolour);
 
   for(var i = ripples.length - 1; i >= 0; i--){
     ripples[i].display();
@@ -49,13 +58,13 @@ function Ripples(x, y) {
   this.d = 0;
 
   this.display = function() {
-    stroke(0);
+    // stroke(0);
     ellipse(this.x, this.y, this.d);
     ellipse(this.x, this.y, this.d * .75);
     ellipse(this.x, this.y, this.d * .5);
   }
   this.move = function(){
-    this.d= this.d += Size;
+    this.d= this.d += Speed;
   }
 
   this.isFinished = function(){
@@ -69,3 +78,10 @@ function Ripples(x, y) {
 
   
 }
+
+// things I want to do
+// customizable background colour
+// customizable stroke colour
+// customizable stroke size
+// customizing the ammount of ripples 1-3
+// customize Speed
